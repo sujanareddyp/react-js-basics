1c279a2cfcd79e6144ebef253353ca360f51283d
sujanareddyp <sujanaachu@gmail.com>
2019-02-25 15:35:02 +0530

    message

--- src/Child.js
+++ src/Child.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 class Child extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            simply: "xxx"
-        }
-    }
+    
     chanheProps = () => {
-        this.props.bb("msg2", "Get lost")
+        this.props.bb("msg4", "Get lost")
     }
     render(){
         return(
